ac82c365e8689be7b50a121e1436b1fd3065bb7f
netó! <82977733+lucianoocria@users.noreply.github.com>
2022-03-25 09:15:55 -0300

    Add files via upload

--- lampadonabala/js.js
+++ lampadonabala/js.js
@@ -1,25 +1,25 @@
-let int = document.createElement("img");
-int.src = "imagens/offoff.png";
-document.body.appendChild(int);
+let interruptor = document.createElement("img");
+interruptor.src = "imagens/offoff.png";
+document.body.appendChild(interruptor);
 let lampada = document.createElement("img");
 lampada.src = "imagens/lampadaoff.png";
-document.body.appendChild(lampada).style.margin = "0% 0% 0% 25%"
+document.body.appendChild(lampada).style.margin = "0em 0em 0em 30em"
 document.body.style.backgroundColor = "#000";
-let estado = "desligado";
-int.onclick = function () {
-    if (estado === "ligado") {
-        int.src = "imagens/onon.png";
+let base = "desligado";
+interruptor.onclick = function () {
+    if (base === "ligado") {
+        interruptor.src = "imagens/onon.png";
         lampada.src = "imagens/lampadaon.png";
         document.body.style.backgroundColor = "#FFF";
     }
     else {
-        int.src = "imagens/offoff.png";
+        interruptor.src = "imagens/offoff.png";
         lampada.src = "imagens/lampadaoff.png";
         document.body.style.backgroundColor = "#000";
     }
     retornar();
 }
 function retornar() {
-    estado === "desligado" ? estado = "ligado" : estado = "desligado";
-    return estado;
+    base === "desligado" ? base = "ligado" : base = "desligado";
+    return base;
 }
